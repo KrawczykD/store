65be0492f18cf71248d8417c8cef55ea41737832
Damian Krawczyk <damiankrawczyk23@gmail.com>
2020-06-15 19:41:48 +0100

    col

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ import './App.css';
 class App extends React.Component {
 
     state = {
-      colection : "electric",
+      colection : "test",
       archive: false,
 
       adviceNumber: null,
