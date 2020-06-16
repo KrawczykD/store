792c55050dcf20d1a87d9b2b4e43077573f6c2b4
Damian Krawczyk <damiankrawczyk23@gmail.com>
2020-06-16 18:50:10 +0100

    Updates

---
+++ precache-manifest.16ddc14480f0ff658096b8c54fb7b807.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d01383558ae138031bb2ebc714a2851",
+    "url": "/store/index.html"
+  },
+  {
+    "revision": "f539566ecf1dbf150705",
+    "url": "/store/static/css/main.b102204c.chunk.css"
+  },
+  {
+    "revision": "24b0306c465c5bdbb738",
+    "url": "/store/static/js/2.c9db2c4e.chunk.js"
+  },
+  {
+    "revision": "f32f3f2739f945f6e8e72f406ee3a70f",
+    "url": "/store/static/js/2.c9db2c4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f539566ecf1dbf150705",
+    "url": "/store/static/js/main.19cd808e.chunk.js"
+  },
+  {
+    "revision": "f84de9975fba0e406c1b",
+    "url": "/store/static/js/runtime-main.51f1e728.js"
+  }
+]);
